4915649f6348b4b6dacda2c7cce6bf0cba564feb
s0zzang <dlth8888@gmail.com>
2024-11-29 14:24:57 +0900

    Style: CSS 변수 등록

--- src/styles/Variables.tsx
+++ src/styles/Variables.tsx
@@ -1,1 +1,40 @@
-// Variables
+const variables = {
+  colors: {
+    primary: '#FFB40F',
+    primary900: '#FE7108',
+    primary800: '#FE900C',
+    primary700: '#FFA10D',
+    primary500: '#FFC215',
+    primary400: '#FFCB2E',
+    primary300: '#FFD652',
+    primary200: '#FFE084',
+    primary100: '#FFECB4',
+    primary50: '#FFECB4',
+
+    gray900: '#737980',
+    gray800: '#5D5D5D',
+    gray700: '#717171',
+    gray600: '#9A9A9A',
+    gray500: '#B9B9B9',
+    gray400: '#DDDDDD',
+    gray300: '#ECECEC',
+    gray200: '#F4F4F4',
+    gray100: '#F9F9F9',
+    white: '#fff',
+  },
+
+  size: {
+    max: '2.2rem',
+    extra: '2rem',
+    large: '1.8rem',
+    big: '1.6rem',
+    medium: '1.4rem',
+    small: '1.2rem',
+    min: '1rem',
+  },
+
+  layoutPadding: '1.6rem',
+  TransitionDuration: '0.3s',
+};
+
+export default variables;
