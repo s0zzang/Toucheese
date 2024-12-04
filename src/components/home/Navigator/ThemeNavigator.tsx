f2fb7201da3e416aff2f9cb3154ef1ebd4d3c367
woojoung <ywj981217@naver.com>
2024-12-04 15:25:35 +0900

    Cont: 선택 시 마름모 표시 추가

--- src/components/home/Navigator/ThemeNavigator.tsx
+++ src/components/home/Navigator/ThemeNavigator.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 type Theme = '전체' | '몽환' | '내추럴' | '러블리' | '시크' | '청순' | '상큼';
 
-/** home 에서 사용되는 네비게이션바 */
+/** home 에서 사용되는 테마셀렉터 */
 const ThemeNavigator = () => {
   const [activeTheme, setActiveTheme] = useState<Theme>('전체');
   const themes: Theme[] = ['전체', '몽환', '내추럴', '러블리', '시크', '청순', '상큼'];
@@ -33,18 +33,34 @@ const NavStyle = styled.nav`
   position: fixed;
   left: 0;
   right: 0;
+  padding: 1.4rem 1.2rem;
 `;
 
 const ThemeListStyle = styled.ul`
   display: flex;
   list-style: none;
   justify-content: space-between;
+  align-items: center;
   cursor: pointer;
 `;
+
 // ThemeNavigator test 코드와 연관되어 있음
 const ThemeButtonStyle = styled.button<{ isActive: boolean }>`
-  padding: 1.2rem 1rem;
+  text-align: center;
   font-size: ${variables.size.big};
   color: ${(props) => (props.isActive ? `${variables.colors.white}` : `${variables.colors.gray500}`)};
   font-weight: ${(props) => (props.isActive ? 'bold' : 'normal')};
+  position: relative;
+
+  &::after {
+    content: '';
+    display: ${(props) => (props.isActive ? 'block' : 'none')};
+    width: 4px;
+    height: 4px;
+    background-color: ${variables.colors.primary};
+    position: absolute;
+    left: 110%;
+    bottom: 1.8rem;
+    transform: translateX(-50%) rotate(45deg);
+  }
 `;
