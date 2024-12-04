086b85ecb1a7e492e012f756f008c49fd46e0131
전희선 <designh2sun@gmail.com>
2024-12-04 13:49:43 +0900

    🚧 cont: BASE_URL 경로 수정

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,7 +3,7 @@
 // import 'whatwg-fetch';
 
 // 테스트 전 공통적으로 사용할 설정
-globalThis.BASE_URL = 'http://localhost:3000';
+globalThis.BASE_URL = 'http://localhost:5173';
 
 // 필요하면 Jest 스타일의 매처 확장
 import '@testing-library/jest-dom';
