85684840123dfee821a7d68552f11da4feff8dca
woojoung <ywj981217@naver.com>
2024-12-04 17:26:54 +0900

    Cont: priceRangeInput 컴포넌트 생성

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
-import ThemeNavigator from '@components/home/Navigator/ThemeNavigator';
+import ThemeNavigator from '@components/Navigator/ThemeNavigator';
+import PriceRangeInput from '@components/PriceRangeInput/PriceRangeInput';
 
 const Home = () => {
   return (
     <>
-      <ThemeNavigator />
+      {/* <ThemeNavigator /> */}
+      <PriceRangeInput />
     </>
   );
 };
