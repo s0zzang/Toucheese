e9a49beeb33d119d11cec43a60c38af12343a52a
s0zzang <dlth8888@gmail.com>
2024-12-05 14:13:51 +0900

    Feat: 지역, 날짜 설정 모달 페이지 구현 (기능 미완)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,14 @@
+import useModal from '@hooks/useModal';
+import LocalDateSelectionModal from './LocalDateSelectionModal';
+
 const Home = () => {
-  return <></>;
+  const modal = useModal();
+  return (
+    <>
+      <button onClick={() => modal.open()}>지역 날짜 선택 모달 열기</button>
+      <LocalDateSelectionModal modalId={1} />
+    </>
+  );
 };
 
 export default Home;
