99fbf2ddbd88c6fdec0f8875f60b2822d4208cde
전희선 <designh2sun@gmail.com>
2024-12-05 14:46:28 +0900

    🔊 test: 공통 버튼 컴포넌트 테스트 코드 수정

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { expect, it, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { expect, it } from 'vitest';
 import '@testing-library/jest-dom';
 import Button from './Button';
 
@@ -8,10 +8,10 @@ it('render with default props', () => {
   render(<Button text="Button Click" />);
 
   /**렌터링 버튼에서 Button Click 찾기 */
-  const button = screen.getByText('Button Click');
+  // const buttonClick = screen.getByText('Button Click');
 
   /**화면에 존재하는지 확인 */
-  expect(button).toBeInTheDocument();
+  const button = screen.getByRole('button');
 
   /** 버튼의 속성 type이 기본 button type인지 확인 */
   expect(button).toHaveAttribute('type', 'button');
