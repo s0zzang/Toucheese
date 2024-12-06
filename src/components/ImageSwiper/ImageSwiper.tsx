235931a6e7b3c00848481685f12e18a230fe132b
지민 <aksendev@gmail.com>
2024-12-06 13:09:01 +0900

    Feat: 이미지 슬라이드 구현

---
+++ src/components/ImageSwiper/ImageSwiper.tsx
@@ -0,0 +1,26 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { css } from '@emotion/react';
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react';
+import 'swiper/css';
+import { Mousewheel } from 'swiper/modules';
+
+interface ImageSwiperProps extends SwiperProps {
+  images: string[];
+}
+
+const ImageSwiper: React.FC<ImageSwiperProps> = ({ images, modules = [Mousewheel], mousewheel = { forceToAxis: true, sensitivity: 1 }, spaceBetween = 3, slidesPerView = 4, ...props }) => {
+  return (
+    <Swiper modules={modules} mousewheel={mousewheel} spaceBetween={spaceBetween} slidesPerView={slidesPerView} {...props}>
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <img css={imageStyle} src={image} alt={`이미지 ${index + 1}`} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default ImageSwiper;
+
+const imageStyle = css``;
