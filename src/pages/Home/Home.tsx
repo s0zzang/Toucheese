035ff31a049ca53fa1749dc30ad8951b2649e09a
JWJung-99 <39busy@naver.com>
2024-12-06 17:47:28 +0900

    Cont: 메인 페이지 구현 중

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,44 @@
-/** @jsxImportSource @emotion/react */
 import BookingSearchContainer from '@components/BookingSearchContainer/BookingSearchContainer';
+import BottomSheet from '@components/BottomSheet/BottomSheet';
 import Button from '@components/Button/Button';
 import Filter from '@components/Filter/Filter';
 import ThemeNavigator from '@components/Navigator/ThemeNavigator';
 import StudioList from '@components/Studio/StudioList';
 import styled from '@emotion/styled';
 import variables from '@styles/Variables';
+import { useEffect, useRef, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import LocalDateSelectionModal from './LocalDateSelectionModal';
 
+interface IFixedProps {
+  isFixed: boolean;
+}
+
 const Home = () => {
   const [searchParams] = useSearchParams();
+  const [isFixed, setIsFixed] = useState(false);
+  const navigatorRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (navigatorRef.current) {
+        const rect = navigatorRef.current.getBoundingClientRect();
+        const initialTop = rect.top;
+        if (rect.top <= 0) setIsFixed(true);
+        else if (rect.top >= initialTop) setIsFixed(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <>
       <SectionStyle>
         <BookingSearchContainer />
 
-        <NavigatorStyle>
+        <NavigatorStyle id="navigator" ref={navigatorRef} isFixed={isFixed}>
           <ThemeNavigator />
           <FilterBox>
             <Button text="" type="reset" variant="gray" icon={<img src="/img/icon-reset.svg" alt="필터 초기화" />} />
@@ -32,16 +53,17 @@ const Home = () => {
         </ListStyle>
       </SectionStyle>
       <LocalDateSelectionModal modalId={1} />
+      <BottomSheet />
     </>
   );
 };
 
 const SectionStyle = styled.section``;
 
-const NavigatorStyle = styled.div`
+const NavigatorStyle = styled.div<IFixedProps>`
   width: 100%;
-  position: absolute;
-  top: 11.8rem;
+  position: ${(props) => (props.isFixed ? 'fixed' : 'absolute')};
+  top: ${(props) => (props.isFixed ? '0' : '11.8rem')};
   left: 0;
   right: 0;
   z-index: 9;
