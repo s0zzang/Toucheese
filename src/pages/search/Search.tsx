ed41f1253ddc7d1db7e553774eaed43525c75a5e
지민 <aksendev@gmail.com>
2024-12-06 13:24:52 +0900

    Feat: 검색 페이지 구현

---
+++ src/pages/search/Search.tsx
@@ -0,0 +1,36 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { css } from '@emotion/react';
+import TrendingStudios from '@pages/search/components/TrendingStudios';
+import RecentSearches from '@pages/search/components/RecentSearches';
+import SearchBar from '@pages/search/components/SearchBar';
+import BackButton from './components/BackButton';
+
+const Search: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleSearch = (term: string) => {
+    navigate(`/search/results?q=${encodeURIComponent(term)}`); //임시
+  };
+
+  return (
+    <div>
+      <div css={searchHeaderStyle}>
+        <BackButton to="/" replace={true} ariaLabel="메인화면으로 돌아가기" />
+        <SearchBar onSearch={handleSearch} />
+      </div>
+      <RecentSearches onSearch={handleSearch} />
+      <TrendingStudios />
+    </div>
+  );
+};
+
+export default Search;
+
+const searchHeaderStyle = css`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 1.6rem;
+`;
