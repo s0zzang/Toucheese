2ba1af814a3c0e02447c80bfcfafc9f7ba3724d7
전희선 <designh2sun@gmail.com>
2024-12-06 14:27:41 +0900

    ✨ feat: home filter button 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,24 +5,29 @@ import variables from '@styles/Variables';
 import { useSearchParams } from 'react-router-dom';
 import useModal from '@hooks/useModal';
 import LocalDateSelectionModal from './LocalDateSelectionModal';
+import Filter from '@components/Filter/Filter';
+import Button from '@components/Button/Button';
 
 const Home = () => {
   const [searchParams] = useSearchParams();
   const modal = useModal();
 
-
   return (
     <>
       <section>
         <button onClick={() => modal.open()}>지역 날짜 선택 모달 열기</button>
-        
+
         <NavigatorStyle>
           <ThemeNavigator />
-          <div>필터 영역</div>
+          <FilterBox>
+            <Button text="" type="reset" variant="gray" icon={<img src="./img/icon-reset.svg" alt="필터 초기화" />} />
+            <Filter text="인기순" />
+            <Filter text="가격대" />
+            <Filter text="매장정보" />
+          </FilterBox>
         </NavigatorStyle>
-  
+
         <StudioList />
-        
       </section>
       <LocalDateSelectionModal modalId={1} />
     </>
@@ -30,13 +35,23 @@ const Home = () => {
 };
 
 const NavigatorStyle = styled.div`
-  box-shadow: inset 0 0 10px red;
   width: calc(100% + 2 * ${variables.layoutPadding});
   position: sticky;
   top: 0;
   left: 0;
   right: 0;
   margin-left: calc(-1 * ${variables.layoutPadding});
+`;
+
+const FilterBox = styled.div`
+  padding: 1.2rem 0rem 1.2rem 1.6rem;
+  display: flex;
+  gap: 0.6rem;
+  box-shadow: 0 0 2px ${variables.colors.gray500};
+
+  button:first-of-type {
+    margin-right: 1rem;
+  }
 `;
 
 export default Home;
