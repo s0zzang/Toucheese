29a86dc741b3b2d04189cd5bbde110aa2a4cdd71
s0zzang <dlth8888@gmail.com>
2024-12-07 14:55:33 +0900

    Cont: 지역 및 날짜 선택 모달 - 적용하기 버튼시 params로 값 전달 (날짜만 완료)

--- src/pages/Home/components/LocalDateSelectionModal.tsx
+++ src/pages/Home/components/LocalDateSelectionModal.tsx
@@ -6,35 +6,59 @@ import styled from '@emotion/styled';
 import useModal from '@hooks/useModal';
 import useBottomSheetState from '@store/useBottomSheetStateStroe';
 import variables from '@styles/Variables';
-import LocalDateBottomSheet from './DateBottomSheet';
+import { useState } from 'react';
+import DateBottomSheet from './DateBottomSheet';
+import { useNavigate } from 'react-router-dom';
 
 const LocalDateSelectionModal = ({ modalId }: { modalId: number }) => {
+  const [selectedDate, setSelectedDate] = useState({ date: '', time: '' });
   const { openBottomSheet } = useBottomSheetState();
+  const navigate = useNavigate();
+
   const modal = useModal(modalId);
   const buttons = [
     {
       text: '적용하기',
       event: () => {
+        setParams();
         modal.close();
       },
     },
   ];
 
+  const setParams = () => {
+    const currentParams = new URLSearchParams(window.location.search);
+    currentParams.set('requestedDateTime', `${selectedDate.date}${selectedDate.time}`);
+    navigate(`?${currentParams.toString()}`);
+  };
+
+  const changeformatDateForUi = ({ date, time }: { date: string; time: string }) => {
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+    const [year, month, day] = date.split('-');
+    const dayOfWeek = week[new Date(date).getDay()];
+
+    // UI를 위한 포맷 변경
+    const selectedDateForUi = `${year}년 ${month}월 ${day}일 (${dayOfWeek})`;
+    const selectedTimeForUi = `${time.split(':')[0].slice(1)}시`;
+
+    return `${selectedDateForUi} ${selectedTimeForUi === '00시' ? '' : selectedTimeForUi}`;
+  };
+
   const handleOpenLocation = () => {};
-  const handleOpenDate = () => openBottomSheet(<LocalDateBottomSheet />, '');
+  const handleOpenDate = () => openBottomSheet(<DateBottomSheet setSelectedDate={setSelectedDate} />, '');
 
   return (
     <>
       <Modal title="지역, 날짜 선택" buttons={buttons} size="full">
         <>
-          <InputBoxStyled>
+          <InputBoxStyle>
             <button type="button" onClick={handleOpenLocation}>
               지역 선택
             </button>
             <button type="button" onClick={handleOpenDate}>
-              예약 날짜 선택
+              {selectedDate.date ? changeformatDateForUi(selectedDate) : '예약 날짜 선택'}
             </button>
-          </InputBoxStyled>
+          </InputBoxStyle>
           <BottomSheet />
         </>
       </Modal>
@@ -44,7 +68,7 @@ const LocalDateSelectionModal = ({ modalId }: { modalId: number }) => {
 
 export default LocalDateSelectionModal;
 
-const InputBoxStyled = styled.div`
+const InputBoxStyle = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
