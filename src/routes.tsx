ab70cfa0531fb46d0b4b0639b173084e45e40b02
woojoung <ywj981217@naver.com>
2024-12-12 17:08:14 +0900

    Conf : 이미지추가 & 파일 경로 및 라우팅

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,4 @@
 import Home from '@pages/Home/Home';
-import ReviewPage from '@pages/reviews/ReviewPage';
 import Search from '@pages/search/Search';
 import SearchResults from '@pages/search/SearchResult';
 import StudioMain from '@pages/Studio/StudioMain/StudioMain';
