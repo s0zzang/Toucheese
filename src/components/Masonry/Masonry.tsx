02ea1e2e9bda44b6cb79ac982d11e085b5490185
s0zzang <dlth8888@gmail.com>
2024-12-18 13:12:35 +0900

    Cont: masonry 각 요소에 클릭 이벤트 전달을 위한 rest 추가

--- src/components/Masonry/Masonry.tsx
+++ src/components/Masonry/Masonry.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
-const MasonryList = ({ children, gap = '.2rem', breakPoints = { 300: 2, 1024: 4 } }: { children: ReactNode; gap?: string; breakPoints?: { [key: number]: number } }) => {
+const MasonryList = ({ children, gap = '.2rem', breakPoints = { 300: 2, 1024: 4 }, ...rest }: { children: ReactNode; gap?: string; breakPoints?: { [key: number]: number } }) => {
   return (
     <>
       <ResponsiveMasonry columnsCountBreakPoints={breakPoints}>
-        <Masonry gutter={gap}>{children}</Masonry>
+        <Masonry gutter={gap} {...rest}>
+          {children}
+        </Masonry>
       </ResponsiveMasonry>
     </>
   );
--- src/pages/Studio/StudioPortfolio/StudioPortfolio.tsx
+++ src/pages/Studio/StudioPortfolio/StudioPortfolio.tsx
@@ -8,13 +8,18 @@ const StudioPortfolio = () => {
   // 포트폴리오 api 개발 전이라 스튜디오 리스트에서 이미지 추출하여 예시 작성
   const { data } = useGetStudios(1, 'filter', searchParams + '');
   const datas = data?.content[12].portfolios;
+  const handleClick = () => {
+    console.log('click');
+  };
 
   return (
     <>
       {datas && (
         <MasonryList>
           {datas.map(({ url, studio, id }) => (
-            <img key={`${studio}-${id}`} src={url} alt={`${studio}-${id}`} />
+            <div key={`${studio}-${id}`} onClick={handleClick}>
+              <img src={url} alt={`${studio}-${id}`} />
+            </div>
           ))}
         </MasonryList>
       )}
