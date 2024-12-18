876c55a54e7f275ea9481751cea91da31732e41b
s0zzang <dlth8888@gmail.com>
2024-12-18 13:16:39 +0900

    Style: masonry 각 요소에 스타일 추가 - 포인터 커서, 너비 지정

--- src/components/Masonry/Masonry.tsx
+++ src/components/Masonry/Masonry.tsx
@@ -1,11 +1,24 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
 import { ReactNode } from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 const MasonryList = ({ children, gap = '.2rem', breakPoints = { 300: 2, 1024: 4 }, ...rest }: { children: ReactNode; gap?: string; breakPoints?: { [key: number]: number } }) => {
+  const masonryItem = css`
+    cursor: pointer;
+
+    img {
+      width: 100%;
+      height: auto;
+      vertical-align: top;
+    }
+  `;
+
   return (
     <>
       <ResponsiveMasonry columnsCountBreakPoints={breakPoints}>
-        <Masonry gutter={gap} {...rest}>
+        <Masonry css={masonryItem} gutter={gap} {...rest}>
           {children}
         </Masonry>
       </ResponsiveMasonry>
