c7658e62753b0f51273d7021680bf981d5a7568c
s0zzang <dlth8888@gmail.com>
2025-01-02 14:02:13 +0900

    Refactor: useQuery 적용

--- src/pages/Studio/StudioPortfolio/StudioPortfolio.tsx
+++ src/pages/Studio/StudioPortfolio/StudioPortfolio.tsx
@@ -5,22 +5,25 @@ import Header from '@components/Header/Header';
 import MasonryList from '@components/Masonry/Masonry';
 import EmptyMessage from '@components/Message/EmptyMessage';
 import StudioNavigator from '@components/Navigator/StudioNavigator';
-import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 import useModal from '@hooks/useModal';
 import { useDimSwiperStore } from '@store/useDimSwiper';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import { IPortfolio } from 'types/types';
+import { IPortfolio, IStudioRes } from 'types/types';
 import DimmedModal from '../components/DimmedModal';
 import PortfolioSwiper from './PortfolioSwiper';
+
+interface IPortfolioResponse {
+  menuIdList: number[];
+  menuNameList: string[];
+  portfolioDtos: IStudioRes<IPortfolio>;
+  studioName: string;
+}
 
 const StudioPortfolio = () => {
   const { _id } = useParams() as { _id: string };
   const { open } = useModal(1);
-
-  const [data, setData] = useState<IPortfolio[]>([]);
-  const [studioName, setStudioName] = useState('');
-  const [menuNames, setMenuNames] = useState<string[]>();
 
   const setSelectedId = useDimSwiperStore((state) => state.setSelectedId);
 
@@ -37,65 +40,66 @@ const StudioPortfolio = () => {
           'Content-Type': 'application/json',
         },
       });
-      const data = await response.json();
-      setData(data.portfolioDtos.content);
-      setMenuNames(data.menuNameList);
-      setStudioName(data.studioName);
+      return await response.json();
     } catch (err) {
       console.error('Failed to fetch data');
     }
   };
 
-  useEffect(() => {
-    fetchPortfolio();
-  }, []);
+  const { data, isSuccess } = useQuery<IPortfolioResponse>({
+    queryKey: ['portfolio', _id],
+    queryFn: fetchPortfolio,
+    staleTime: 1000 * 60 * 10, // 10분
+  });
 
   return (
     <>
-      <Header title={studioName} />
+      <Header title={isSuccess ? data.studioName : ''} />
       <StudioNavigator _id={_id} />
 
-      <FilterBoxStyle>
+      <ul css={FilterBoxStyle}>
         <li>
           <Button text="전체" width="fit" size="small" variant="white" />
         </li>
-        {menuNames &&
-          menuNames.map((menu) => (
+        {isSuccess &&
+          data.menuNameList.map((menu) => (
             <li key={menu}>
               <Button text={menu} width="fit" size="small" variant="white" />
             </li>
           ))}
-      </FilterBoxStyle>
+      </ul>
 
-      <ListStyle>
-        {data.length ? (
-          <MasonryList>
-            {data.map(({ url, studio, id }) => (
-              <div key={`${studio}-${id}`} onClick={() => handleClick(id)}>
-                <img src={url} alt={`${studio}-${id}`} />
-              </div>
-            ))}
-          </MasonryList>
+      <div css={ListStyle}>
+        {isSuccess ? (
+          data.portfolioDtos.content.length ? (
+            <MasonryList>
+              {data.portfolioDtos.content.map(({ url, studio, id }) => (
+                <div key={`${studio}-${id}`} onClick={() => handleClick(id)}>
+                  <img src={url} alt={`${studio}-${id}`} />
+                </div>
+              ))}
+            </MasonryList>
+          ) : (
+            <EmptyMessage message="포트폴리오가 없습니다." />
+          )
         ) : (
-          <EmptyMessage message="포트폴리오가 없습니다." />
+          <EmptyMessage message="문제가 발생했습니다. 잠시 후 다시 시도해주세요." />
         )}
-      </ListStyle>
+      </div>
 
-      <DimmedModal>
-        <PortfolioSwiper data={data} studioName={studioName} />
-      </DimmedModal>
+      {<DimmedModal>{isSuccess && <PortfolioSwiper data={data.portfolioDtos.content} studioName={data.studioName} />}</DimmedModal>}
     </>
   );
 };
 
 export default StudioPortfolio;
 
-const FilterBoxStyle = styled.ul`
+const FilterBoxStyle = css`
   display: flex;
   gap: 0.6rem;
   margin: 1.2rem 0;
 `;
 
-const ListStyle = styled.div`
+const ListStyle = css`
   min-height: 50vh;
 `;
