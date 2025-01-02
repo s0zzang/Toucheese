3fcd45abca68c3312c4435510c314b8ef94946d0
woojoung <ywj981217@naver.com>
2025-01-02 13:46:53 +0900

    Feat : 로그인 페이지 타이틀 및 공통 인풋 컴포넌트 파일 생성

--- src/pages/User/Login.tsx
+++ src/pages/User/Login.tsx
@@ -1,13 +1,40 @@
+/** @jsxImportSource @emotion/react */
 import Header from '@components/Header/Header';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import { css } from '@emotion/react';
+import { TypoTitleSmS } from '@styles/Common';
+import Input from '@components/Input/Input';
 
 const Login = () => {
   return (
     <>
-      <Header title="로그인/회원가입" />
-      <Link to="/user/signup">회원가입</Link>
+      <div
+        css={css`
+          margin-bottom: 3rem;
+        `}
+      >
+        <Header title="로그인/회원가입" />
+      </div>
+
+      <div css={LoginPageDesStyle}>
+        <h1>터치즈에서 간편하게</h1>
+        <p>내 인생 사진관 찾고 예약까지!</p>
+      </div>
+
+      <Input labelName="이메일(아이디)" type="email" placeholder="toucheese.gmail.com" />
+      <Input labelName="비밀번호" type="password" placeholder="8자 이상의 비밀번호" />
     </>
   );
 };
 
 export default Login;
+
+const LoginPageDesStyle = css`
+  h1 {
+    ${TypoTitleSmS}
+  }
+  p {
+    font-size: 1.8rem;
+  }
+  margin-bottom: 3.4rem;
+`;
