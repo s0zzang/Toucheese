70432b633e0d3eff40c1ea62992c3d341ec4a97d
s0zzang <dlth8888@gmail.com>
2025-01-08 13:21:05 +0900

    Done: 날짜 선택 컴포넌트 기능 변경 (단일 -> 다중)

--- src/store/useSelectTime.ts
+++ src/store/useSelectTime.ts
@@ -1,11 +1,27 @@
 import { create } from 'zustand';
 
 interface TimeState {
-  time: string;
-  setTime: (date: string) => void;
+  time: Set<string>;
+  setTime: (date: string, type: string) => void;
 }
 
 export const useSelectTimeStore = create<TimeState>((set) => ({
-  time: 'T00:00:00',
-  setTime: (newTime) => set({ time: newTime }),
+  time: new Set(),
+  setTime: (newTime: string, type: string) =>
+    set((state) => {
+      const updatedTime = new Set(state.time);
+
+      if (type === 'filter') {
+        if (newTime === '') updatedTime.clear();
+        if (updatedTime.has(newTime)) updatedTime.delete(newTime);
+        else updatedTime.add(newTime);
+      }
+
+      if (type === 'reservation') {
+        updatedTime.clear();
+        updatedTime.add(newTime);
+      }
+
+      return { time: updatedTime };
+    }),
 }));
