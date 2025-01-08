bc9b0dfa80a66085de7b0282b68cbeb8b8122829
s0zzang <dlth8888@gmail.com>
2025-01-08 13:16:54 +0900

    Done: 모달 고도화 - 모달 내 버튼(너비,색상) 동적 반영, 모달 활성화 시 html 요소 스타일 변경(overflow), 모달 내 콘텐츠에 스크롤이 생기는 경우 스타일 개선

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,6 +6,7 @@ import useModal from '@hooks/useModal';
 import { useModalStore } from '@store/useModalStore';
 import { Hidden, TypoBodyMdR, TypoTitleSmS } from '@styles/Common';
 import variables from '@styles/Variables';
+import { useEffect } from 'react';
 
 interface ModalProp {
   type: 'default' | 'dimmed' | 'fullscreen';
@@ -13,7 +14,12 @@ interface ModalProp {
   children: JSX.Element | string;
   modalId?: number;
   withBtn?: boolean;
-  buttons?: { text: string; event: () => void }[];
+  buttons?: {
+    text: string;
+    event: () => void;
+    variant?: 'black' | 'gray';
+    width?: 'max' | 'fit';
+  }[];
 }
 
 interface IModalStyle {
@@ -40,11 +46,25 @@ interface IContentStyle {
  *  - 태그 전달 : `<Modal title="모달2" buttons={buttons}> <p>모달2</p> </Modal>`
  *  - 모달 내 버튼 : {text: string, event: MouseEventHandler<HTMLButtonElement>}[]
  */
-const Modal = ({ modalId = 1, type = 'default', title, children, withBtn = true, buttons = [] }: ModalProp) => {
+const Modal = ({
+  modalId = 1,
+  type = 'default',
+  title,
+  children,
+  withBtn = true,
+  buttons = [],
+}: ModalProp) => {
   const modals = useModalStore((state) => state.modals);
   const isOpen = modals[modalId];
   const { close } = useModal(modalId);
   const handleClose = () => close();
+
+  useEffect(() => {
+    // 모달이 활성화되면 html에 생기는 전체 스크롤 방지
+    const htmlStyle = window.document.documentElement.style;
+    if (isOpen) htmlStyle.overflow = 'hidden';
+    else htmlStyle.overflow = 'auto';
+  }, [isOpen]);
 
   return (
     isOpen && (
@@ -70,7 +90,20 @@ const Modal = ({ modalId = 1, type = 'default', title, children, withBtn = true,
         )}
         <ContentsStyle type={type}>{children}</ContentsStyle>
 
-        {withBtn && <ButtonBoxStyle>{buttons?.map((btn) => <Button key={btn.text} variant="black" onClick={btn.event} text={btn.text} disabled={false} />)}</ButtonBoxStyle>}
+        {withBtn && (
+          <ButtonBoxStyle>
+            {buttons?.map(({ text, variant = 'black', event, width = 'max' }) => (
+              <Button
+                key={text}
+                variant={variant}
+                onClick={event}
+                text={text}
+                disabled={false}
+                width={width}
+              />
+            ))}
+          </ButtonBoxStyle>
+        )}
       </ModalStyle>
     )
   );
@@ -82,11 +115,13 @@ const ModalStyle = styled.section<IModalStyle>`
   position: fixed;
   z-index: 99;
   inset: 0;
-  background: ${(props) => (props.type !== 'fullscreen' ? 'rgba(0,0,0,0.85)' : variables.colors.white)};
-  padding: 0 2rem 4.8rem;
+  background: ${(props) =>
+    props.type !== 'fullscreen' ? 'rgba(0,0,0,0.85)' : variables.colors.white};
+  padding: 5.2rem 2rem 10rem;
   display: flex;
   flex-direction: column;
   justify-content: ${(props) => (props.type !== 'fullscreen' ? '' : 'space-between')};
+  overflow: hidden auto;
 `;
 
 const TitleStyle = styled.div<ITitleStyle>`
@@ -94,14 +129,22 @@ const TitleStyle = styled.div<ITitleStyle>`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative;
   color: ${(props) => props.type === 'dimmed' && variables.colors.white};
+  min-height: 5.2rem;
+  position: fixed;
+  top: 0;
+  left: 2rem;
+  right: 2rem;
+  z-index: 1;
 `;
 
 const CloseBtnStyle = styled.button<ICloseBtnStyle>`
   width: 2.4rem;
   aspect-ratio: 1/1;
-  background: ${(props) => (props.mode === 'fullscreen' ? 'url(/img/icon-arrow-gray800.svg) no-repeat center / 1.1rem 1.9rem' : 'url(/img/icon-close-white.svg) no-repeat center / 1.2rem')};
+  background: ${(props) =>
+    props.mode === 'fullscreen'
+      ? 'url(/img/icon-arrow-gray800.svg) no-repeat center / 1.1rem 1.9rem'
+      : 'url(/img/icon-close-white.svg) no-repeat center / 1.2rem'};
   position: absolute;
   left: ${(props) => props.mode === 'fullscreen' && 0};
   right: ${(props) => props.mode === 'dimmed' && 0};
@@ -111,12 +154,15 @@ const CloseBtnStyle = styled.button<ICloseBtnStyle>`
 const ContentsStyle = styled.div<IContentStyle>`
   padding: ${(props) => props.type === 'fullscreen' && '1rem 0'};
   flex-grow: 1;
-  overflow-y: auto;
 `;
 
 const ButtonBoxStyle = styled.div`
-  padding: 1rem 0;
+  padding: 1rem 1.6rem 4rem;
   display: flex;
   justify-content: space-between;
   gap: 0.8rem;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
 `;
