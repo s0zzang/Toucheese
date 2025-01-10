1a89a3c95fce677247ab71ddd8e15339ff8ccf9e
전희선 <designh2sun@gmail.com>
2025-01-10 14:39:40 +0900

    Done: localStorage data 삭제 로직 추가

--- src/store/useSignupStore.ts
+++ src/store/useSignupStore.ts
@@ -1,14 +1,20 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
-interface SignupState {
+interface PersistedSignupState {
   name: string;
   phone: string;
-  setSignupData: (data: Partial<SignupState>) => void;
+  setSignupData: (data: Partial<PersistedSignupState>) => void;
   clearSignupData: () => void;
+  _persist?: {
+    options: {
+      name: string;
+      storage: Storage;
+    };
+  };
 }
 
-const useSignupStore = create<SignupState>()(
+const useSignupStore = create<PersistedSignupState>()(
   persist(
     (set) => ({
       name: '',
@@ -18,11 +24,19 @@ const useSignupStore = create<SignupState>()(
           ...state,
           ...data,
         })),
-      clearSignupData: () =>
+      clearSignupData: () => {
+        /** 화면 표시값 삭제 */
         set({
           name: '',
           phone: '',
-        }),
+        });
+
+        /** local Storage의 데이터 삭제 */
+        const persistOprions = useSignupStore.getState()._persist?.options;
+        if (persistOprions?.storage) {
+          persistOprions.storage.removeItem(persistOprions.name || 'signup-storage');
+        }
+      },
     }),
     {
       name: 'signup-storage',
