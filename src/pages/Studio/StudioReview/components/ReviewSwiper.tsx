6aefda2694d6e1d8c8df7dd796d6c25ecb00b0b8
woojoung <ywj981217@naver.com>
2025-01-10 13:23:50 +0900

    Feat: 리뷰사진 모아보기 딤처리 정보 출력

--- src/pages/Studio/StudioReview/components/ReviewSwiper.tsx
+++ src/pages/Studio/StudioReview/components/ReviewSwiper.tsx
@@ -4,21 +4,46 @@ import DimSwiper, { SlideImgBox } from '@components/Swiper/DimSwiper';
 import { useState } from 'react';
 import { SwiperSlide } from 'swiper/react';
 import { IReviewImages } from 'types/types';
+import StarRating from './StarRating';
+import { css } from '@emotion/react';
+import { TypoTitleXsM } from '@styles/Common';
 
 const ReviewSwiper = ({ data }: { data: IReviewImages[] }) => {
   const [slideSet, setSlideSet] = useState<IReviewImages[]>([]);
 
+  const renderSlides = () => {
+    return slideSet.map((slide) => (
+      <SwiperSlide key={slide.id} virtualIndex={slide.id}>
+        <p>{slide.userName}</p>
+        <StarRating rating={slide.rating || 0} />
+        <div css={[SlideImgBox]}>
+          <img src={slide.url} alt={`리뷰 이미지 ${slide.id}`} />
+        </div>
+        <div>
+          <h1
+            css={css`
+              ${TypoTitleXsM}
+            `}
+          >
+            {slide.menuName}
+          </h1>
+          <p
+            css={css`
+              opacity: 0.6;
+              margin-bottom: 0.8rem;
+            `}
+          >
+            컷 추가 수정
+          </p>
+          <p>{slide.reviewContent}</p>
+        </div>
+      </SwiperSlide>
+    ));
+  };
+
   return (
     <DimSwiper<IReviewImages> data={data} setSlideSet={setSlideSet}>
-      {slideSet &&
-        slideSet.map(({ id, url }) => (
-          <SwiperSlide key={id} virtualIndex={id}>
-            <div css={[SlideImgBox]}>
-              <img src={url} alt={`리뷰 이미지 ${id}`} />
-            </div>
-            <p>기타 내용 작성</p>
-          </SwiperSlide>
-        ))}
+      {slideSet && renderSlides()}
     </DimSwiper>
   );
 };
