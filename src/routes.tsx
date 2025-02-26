4e3d2732e1dcdf53cd97251bca869d4226a913ce
지민 <aksendev@gmail.com>
2025-02-26 14:52:25 +0900

    Style: 헤더 폰트 두께 변경

--- src/routes.tsx
+++ src/routes.tsx
@@ -149,7 +149,6 @@ const router = createBrowserRouter([
                 path: 'complete',
                 element: <ReservationComplete />,
               },
-              { path: 'npay/callback', element: <ReservationNpayCallback /> },
             ],
           },
         ],
