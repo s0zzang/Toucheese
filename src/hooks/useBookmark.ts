fa7de3bb1a2bcccfe1b4276920edd243cbec6790
JWJung-99 <39busy@naver.com>
2025-02-28 14:37:12 +0900

    Refactor: 에러 바운더리 적용

--- src/hooks/useBookmark.ts
+++ src/hooks/useBookmark.ts
@@ -1,27 +1,33 @@
 // userId === 1 => 로그인/회원가입 시 변경 예정
 const postBookmark = async (userId: number = 1, studioId: number) => {
-  const response = await fetch(`${import.meta.env.VITE_TOUCHEESE_API}/user/bookmark/${userId}/${studioId}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
+  const response = await fetch(
+    `${import.meta.env.VITE_TOUCHEESE_API}/user/bookmark/${userId}/${studioId}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     },
-  });
+  );
 
   if (!response.ok) {
-    console.error('Failed to bookmark');
+    throw new Error('Failed to add bookmark');
   }
 };
 
 const deleteBookmark = async (userId: number = 1, studioId: number) => {
-  const response = await fetch(`${import.meta.env.VITE_TOUCHEESE_API}/user/bookmark/${userId}/${studioId}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
+  const response = await fetch(
+    `${import.meta.env.VITE_TOUCHEESE_API}/user/bookmark/${userId}/${studioId}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     },
-  });
+  );
 
   if (!response.ok) {
-    console.error('Failed to bookmark');
+    throw new Error('Failed to delete bookmark');
   }
 };
 
