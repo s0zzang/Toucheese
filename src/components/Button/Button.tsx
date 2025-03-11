8e9264b08ba053f2b59f14ac93435212bef7e9d3
전희선 <designh2sun@gmail.com>
2025-03-11 21:34:48 +0900

    Feat: 버튼 공통 컴포넌트 reset  small, medium, large 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
 
-import { css, CSSObject } from '@emotion/react';
+import { css, CSSObject, keyframes } from '@emotion/react';
 import { PCLayout, TypoBodyMdM, TypoBodyMdR, TypoBodySmM, TypoTitleXsM } from '@styles/Common';
 import variables from '@styles/Variables';
-import React from 'react';
+import React, { useState } from 'react';
 
 type ButtonVariant =
   | 'primary'
@@ -13,14 +13,18 @@ type ButtonVariant =
   | 'grayWithYellowOutline'
   | 'grayWithGrayOutline';
 
+type ButtonSize = 'xsmall' | 'small' | 'medium' | 'large';
+type IconResetSize = 'small' | 'medium' | 'large';
+
 interface ButtonProps {
-  text: string;
-  variant?: ButtonVariant;
+  text?: string;
+  variant: ButtonVariant;
   disabled?: boolean;
   active?: boolean;
   onClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
-  size?: 'xsmall' | 'small' | 'medium' | 'large';
+  size?: ButtonSize;
+  iconResetSize?: IconResetSize;
   fixed?: boolean;
   width?: 'fit' | 'max';
   iconPosition?: 'non' | 'left' | 'right';
@@ -44,6 +48,14 @@ interface ButtonProps {
     - type="button"
  */
 
+/**  리셋 아이콘 컴포넌트 사용
+ * 
+    - type="reset" 버튼 리셋 타입
+    - iconResetSize="small" 아이콘 리셋 버튼 사이즈
+    - variant="gray" 리셋은 gray300 공통이지만 필수값이므로 전달
+    <Button type="reset" iconResetSize="small" variant="gray" />
+ */
+
 const Button = ({
   icon,
   iconSizeWidth = '2rem',
@@ -59,11 +71,28 @@ const Button = ({
   fixed = false,
   size = 'large',
   style,
+  iconResetSize,
 }: ButtonProps) => {
+  const isIconReset = type === 'reset' && iconResetSize;
+
+  // ----------------------------- 리셋 아이콘 버튼 클릭 여부 및 상태관리 -----------------------------
+  const [isAnimating, setIsAnimating] = useState(false);
+  const handleButtonClick = () => {
+    if (onClick) {
+      onClick();
+    }
+    if (isIconReset) {
+      setIsAnimating(true);
+      setTimeout(() => setIsAnimating(false), 400);
+    }
+  };
+
+  // ----------------------------- 가로값 사이즈 -----------------------------
   const widthStyles = {
     fit: css`
       min-width: fit-content;
       max-width: fit-content;
+      padding: 0 1rem;
     `,
     max: css`
       width: 100%;
@@ -71,7 +100,45 @@ const Button = ({
       ${PCLayout}
     `,
   };
+  // ----------------------------- 아이콘 전용 리셋 버튼 크기 -----------------------------
+  const iconResetSizeStyles = {
+    small: css`
+      width: 3.2rem;
+      height: 3.2rem;
+    `,
+    medium: css`
+      width: 4rem;
+      height: 4rem;
+    `,
+    large: css`
+      width: 4.8rem;
+      height: 4.8rem;
+    `,
+  };
 
+  // ----------------------------- 아이콘 전용 리셋 버튼 패딩값 -----------------------------
+  const iconResetPaddingStyles = {
+    small: css`
+      padding: 0.8rem;
+    `,
+    medium: css`
+      padding: 1rem;
+    `,
+    large: css`
+      padding: 1.2rem;
+    `,
+  };
+  // ----------------------------- 아이콘 리셋 버튼 애니메이팅 -----------------------------
+  const rotateIcon = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+  // ----------------------------------------------------------
   const iconWrapperStyles = css`
     display: flex;
     align-items: center;
@@ -82,13 +149,13 @@ const Button = ({
     ${icon && `img{width:${iconSizeWidth}; height:${iconSizeHeight}}`}
   `;
 
+  // ----------------------------- 버튼 사이즈 -----------------------------
   const sizeStyles = {
     xsmall: css`
       ${TypoBodySmM};
       height: 3.2rem;
       padding: 0 1rem;
     `,
-
     small: css`
       ${TypoBodyMdR};
       height: 3.6rem;
@@ -109,10 +176,10 @@ const Button = ({
     `,
   };
 
+  // ----------------------------- 버튼 기본 스타일 -----------------------------
   const styles = css`
     border: none;
     border-radius: 0.6rem;
-    color: ${variables.colors.gray900};
     cursor: pointer;
     width: 100%;
     display: flex;
@@ -120,32 +187,29 @@ const Button = ({
     justify-content: center;
     text-align: center;
     box-sizing: border-box;
-
     ${sizeStyles[size]}
     ${widthStyles[width]}
 
-
-    // 노란색 버튼
+    // ----------------------------- 노란색 버튼 ----------------------------- 
     ${variant === 'primary' &&
     css`
       background-color: ${variables.colors.primary50};
       border: 0.1rem solid ${variables.colors.primary};
     `}
 
-      // 블랙 버튼
+// ----------------------------- 블랙 버튼 ----------------------------- 
     ${variant === 'black' &&
     css`
       background-color: ${variables.colors.gray900};
       color: ${variables.colors.white};
     `}
     
-    // 기본 회색 버튼
+// ----------------------------- 기본 회색 버튼 ----------------------------- 
     ${variant === 'gray' &&
     css`
       background-color: ${variables.colors.gray400};
       color: ${variables.colors.white};
     `}
-
 
     ${variant === 'gray' &&
     active &&
@@ -155,7 +219,7 @@ const Button = ({
       border: none;
     `}
 
-    // 흰색 버튼
+// ----------------------------- 흰색 버튼 ----------------------------- 
     ${variant === 'white' &&
     css`
       background-color: ${variables.colors.white};
@@ -169,7 +233,7 @@ const Button = ({
       border: 0.1rem solid ${variables.colors.primary500};
     `}
 
-    // 외곽선 노란색 회색 버튼
+ // ----------------------------- 외곽선 노란색 회색 버튼 -----------------------------
     ${variant === 'grayWithYellowOutline' &&
     css`
       background-color: ${variables.colors.gray300};
@@ -185,7 +249,7 @@ const Button = ({
     `}
   
 
-    // 외곽선 진회색 회색버튼
+ // ----------------------------- 외곽선 진회색 버튼 -----------------------------
     ${variant === 'grayWithGrayOutline' &&
     css`
       background-color: ${variables.colors.gray100};
@@ -202,7 +266,7 @@ const Button = ({
     `}
 
 
-
+  // ----------------------------- 버튼 위치 고정 -----------------------------
     ${fixed &&
     css`
       position: fixed;
@@ -214,21 +278,33 @@ const Button = ({
       z-index: 1000;
     `}
 
-${type === 'reset' &&
+// ----------------------------- 아이콘 리셋 스타일 -----------------------------
+    ${type === 'reset' &&
+    iconResetSize &&
     css`
-      width: 3rem;
-      height: 3rem;
-      border-radius: 2rem;
+      ${iconResetSizeStyles[iconResetSize]}
+      ${iconResetPaddingStyles[iconResetSize]}
+      background-color: ${variables.colors.gray300};
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: none;
+      aspect-ratio: 1 / 1;
     `}
 
 
+// ----------------------------- 작은버튼 padding 값 -----------------------------
 ${width === 'fit' &&
     !(size === 'medium' || size === 'large') &&
     css`
       padding: 0 1.4rem;
     `}
 
-
+  // ----------------------------- disabled일 때 기본값 -----------------------------
     ${disabled &&
     css`
       background-color: ${variables.colors.gray400};
@@ -239,10 +315,24 @@ ${width === 'fit' &&
   `;
 
   return (
-    <button type={type} css={[styles, style]} disabled={disabled} onClick={onClick}>
+    <button type={type} css={[styles, style]} disabled={disabled} onClick={handleButtonClick}>
       <div css={iconWrapperStyles}>
-        {icon}
-        {text}
+        {isIconReset ? (
+          <img
+            src="/img/icon-reset.svg"
+            alt="초기화 아이콘"
+            css={[
+              iconResetSizeStyles[iconResetSize],
+              isAnimating &&
+                css`
+                  animation: ${rotateIcon} 0.4s ease-out;
+                `,
+            ]}
+          />
+        ) : (
+          icon
+        )}
+        {!isIconReset && text}
       </div>
     </button>
   );
