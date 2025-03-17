f530af85bffa365b86df08980cdec71a822a082e
s0zzang <dlth8888@gmail.com>
2025-03-17 12:41:03 +0900

    Done: PC Masonry 레이아웃 변경 (4개 -> 2개)

--- src/components/Masonry/Masonry.tsx
+++ src/components/Masonry/Masonry.tsx
@@ -4,7 +4,16 @@ import { css } from '@emotion/react';
 import { ReactNode } from 'react';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
-const MasonryList = ({ children, gap = '.2rem', breakPoints = { 300: 2, 1024: 4 }, ...rest }: { children: ReactNode; gap?: string; breakPoints?: { [key: number]: number } }) => {
+const MasonryList = ({
+  children,
+  gap = '.2rem',
+  breakPoints = { 300: 2, 1024: 2 },
+  ...rest
+}: {
+  children: ReactNode;
+  gap?: string;
+  breakPoints?: { [key: number]: number };
+}) => {
   const masonryItem = css`
     cursor: pointer;
 
