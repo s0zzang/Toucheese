a87c25d8414cbe05c6cecdb38bf39f25135b4f51
s0zzang <dlth8888@gmail.com>
2025-03-21 17:22:31 +0900

    Done: 날짜, 시간 선택 - 지나간 시간 비활성화

--- src/pages/Home/components/SelectTime.tsx
+++ src/pages/Home/components/SelectTime.tsx
@@ -5,7 +5,7 @@ import EmptyMessage from '@components/Message/EmptyMessage';
 import { css } from '@emotion/react';
 import { filterTimes } from '@hooks/useGetAvailableDate';
 import useToast from '@hooks/useToast';
-import { useSelectDateStore } from '@store/useSelectDateStore';
+import { convertToDateFormat, today, useSelectDateStore } from '@store/useSelectDateStore';
 import { useSelectTimeStore } from '@store/useSelectTimeStore';
 import { DividerStyle, Hidden, TypoBodyMdM, TypoBodySmR } from '@styles/Common';
 import variables from '@styles/Variables';
@@ -25,25 +25,42 @@ interface ITimeProp {
 }
 
 const SelectTime = ({ type, availableTimeWithDates }: ITimeProp) => {
+  const { date: selectedDate } = useSelectDateStore();
   const { time: selectedTime, setTime } = useSelectTimeStore();
-  const { date } = useSelectDateStore();
+
+  const getTimes = () => {
+    if (type === 'filter') return filterTimes;
+    if (type === 'reservation') {
+      return availableTimeWithDates?.find((TimeWithDate) => TimeWithDate.date === selectedDate)
+        ?.availableTimeDto;
+    }
+  };
+
+  const morningTimes = useMemo(
+    () => getTimes()?.filter(({ time }) => time < '12:00'),
+    [selectedDate],
+  );
+
+  const afternoonTimes = useMemo(
+    () => getTimes()?.filter(({ time }) => time >= '12:00'),
+    [selectedDate],
+  );
+
+  const isLowerThanCurrentTime = (time: string) => {
+    const newDate = new Date();
+    const currentTime = `${newDate.getHours()}:${newDate.getMinutes()}`;
+    const isTodaySelected = selectedDate === convertToDateFormat(today);
+    return isTodaySelected ? time >= currentTime : true;
+  };
+
   const openToast = useToast();
-  const times =
-    type === 'filter'
-      ? filterTimes
-      : availableTimeWithDates?.find((TimeWithDate) => TimeWithDate.date === date)
-          ?.availableTimeDto;
-
-  const morningTimes = useMemo(() => times?.filter((times) => times.time < '12:00'), [times]);
-  const afternoonTimes = useMemo(() => times?.filter((times) => times.time >= '12:00'), [times]);
-
   const handleTImeClick = (value: string) => {
-    if (!date) return openToast('날짜를 먼저 선택해주세요.');
+    if (!selectedDate) return openToast('날짜를 먼저 선택해주세요.');
     setTime(value, type);
   };
 
   if (type === 'reservation') {
-    if (!times)
+    if (!getTimes())
       return (
         <div css={emptyMessageBox}>
           <EmptyMessage message="예약이 불가능합니다." />
@@ -71,7 +88,7 @@ const SelectTime = ({ type, availableTimeWithDates }: ITimeProp) => {
                       width="max"
                       active={!!selectedTime.length && selectedTime.includes(time)}
                       onClick={() => handleTImeClick(time)}
-                      style={available ? {} : disabledStyle}
+                      style={available && isLowerThanCurrentTime(time) ? {} : disabledStyle}
                     />
                     <h4 css={Hidden}>{time}</h4>
                   </li>
@@ -94,7 +111,7 @@ const SelectTime = ({ type, availableTimeWithDates }: ITimeProp) => {
                       width="max"
                       active={!!selectedTime.length && selectedTime.includes(time)}
                       onClick={() => handleTImeClick(time)}
-                      style={available ? {} : disabledStyle}
+                      style={available && isLowerThanCurrentTime(time) ? {} : disabledStyle}
                     />
                     <h4 css={Hidden}>{time}</h4>
                   </li>
