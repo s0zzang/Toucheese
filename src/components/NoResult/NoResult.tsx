0c4b30bbb6364d4ba06c6219211f502f7e2f304f
JWJung-99 <39busy@naver.com>
2025-04-25 23:33:58 +0900

    Style: 스튜디오 조회 결과 없음 페이지 여백 수정

--- src/components/NoResult/NoResult.tsx
+++ src/components/NoResult/NoResult.tsx
@@ -22,9 +22,10 @@ const DivStyle = ({ bg }: { bg: 'white' | 'gray100' }) => css`
   width: 100%;
   display: flex;
   color: ${variables.colors.gray700};
-  height: ${bg === 'white' ? 'calc(100vh - 8rem)' : 'calc(100vh - 10rem)'};
+  height: ${bg === 'white' ? 'calc(100vh - 19.6rem)' : 'calc(100vh - 10rem)'};
   margin-bottom: -9.6rem;
   background-color: ${variables.colors[bg]};
+  margin-top: ${bg === 'white' && '0.6rem'};
 
   & p {
     flex-grow: 1;
@@ -46,6 +47,7 @@ const DivStyle = ({ bg }: { bg: 'white' | 'gray100' }) => css`
   ${mqMin(breakPoints.pc)} {
     height: ${bg === 'white' ? 'calc(100vh - 13.8rem)' : 'calc(100vh - 21.8rem)'};
     margin-bottom: ${bg === 'gray100' && 'calc(-1 * 12rem)'};
+    margin-top: ${bg === 'white' && 'unset'};
 
     & p {
       &::before {
