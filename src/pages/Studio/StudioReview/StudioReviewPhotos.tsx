39a26e16a6a991d448f254d52e93b93e92746d63
s0zzang <dlth8888@gmail.com>
2025-05-01 16:01:08 +0900

    Done: 딤 스와이퍼(리뷰, 포트폴리오) - tabtrap 적용

--- src/pages/Studio/StudioReview/StudioReviewPhotos.tsx
+++ src/pages/Studio/StudioReview/StudioReviewPhotos.tsx
@@ -101,7 +101,13 @@ const StudioReviewPhotos = () => {
 
           <MasonryList>
             {reviewImages.imageDtos.map(({ id, url }) => (
-              <div key={id} onClick={() => handleClick(id)}>
+              <div
+                key={id}
+                onClick={() => handleClick(id)}
+                onKeyDown={(e) => e.key === 'Enter' && handleClick(id)}
+                tabIndex={0}
+                data-tab="focus"
+              >
                 <picture>
                   <source srcSet={url.replace(/\.(jpg|jpeg|png)$/, '.webp')} type="image/webp" />
                   <img src={url} alt={`리뷰 이미지 ${id}`} />
