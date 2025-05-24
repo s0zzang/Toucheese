1d28825f8d4d2440506a28a2d37bd0cd152685f1
JWJung-99 <39busy@naver.com>
2025-05-24 19:26:08 +0900

    Fix: 빌드 에러 해결

--- src/pages/Studio/StudioReview/StudioReviewWritePage.tsx
+++ src/pages/Studio/StudioReview/StudioReviewWritePage.tsx
@@ -126,6 +126,7 @@ const StudioReviewWritePage = () => {
       }
 
       const data = await response.json();
+      console.log(data);
       alert('리뷰가 등록되었습니다.');
       navigate(`/studio/${_id}/review`);
     } catch (error) {
